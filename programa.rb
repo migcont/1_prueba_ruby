d6ca36f0ad158a737e3275cf2410e894482f84b5
miguel <miguel.contrerastorrejon@gmail.com>
2018-12-12 15:03:14 -0300

    ADD: leyendo la informacion del csv

--- programa.rb
+++ programa.rb
@@ -2,37 +2,50 @@
 #manejo de archivo
 file = File.open('datos.csv', 'r')
 data = file.readlines.map(&:chomp)
+data = data.map { |e| e.split(',') }
 file.close
-puts data
 
+puts data
+puts "---------------------", "\n"
 
 #manejo de datos
+#opcion 1
+def promedio (data)
+
+end
+
+#opcion 2
+def inasistencias (data)
+
+end
+
+
+#opcion3
+def aprobados (data)
+
+end
 
 #opciones de menu
 option = 0
-puts 'notas del curso', "\n"
+puts 'Informacion del curso: ', "\n"
 while option
-	puts 'Ingrese una opcion: ', "\n"
-	puts 'Ingrese 1: para ver el nombre de cada alumno y sus notas'
-	puts 'Ingrese 2: para mostrar la cantidad de inasistencias totales'
-	puts 'Ingrese 3: para mostrar a los alumnos aprobados'
-	puts 'Ingrese 4: para salir'
-
-	option = gets.to_i
-	case option
-		when 1
-		puts '1', "\n"
-		puts promedio
-	when 2
-		puts '2', "\n"
-		puts inasistencias
-	when 3
-		puts '3', "\n"
-		puts aprobados
-	when 4
-		puts 'salir', "\n"
-		exit
-	else
-		puts 'Opcion no valida, ingresa una entre 1 y 4', "\n"
-	end
+  puts 'Ingrese una opcion: ', "\n"
+  puts 'Ingrese 1: para ver el nombre de cada alumno y sus notas'
+  puts 'Ingrese 2: para mostrar la cantidad de inasistencias totales'
+  puts 'Ingrese 3: para mostrar a los alumnos aprobados'
+  puts 'Ingrese 4: para salir'
+  option = gets.to_i
+  case option
+  when 1
+    puts promedio
+  when 2
+    puts inasistencias
+  when 3
+    puts aprobados
+  when 4
+    puts 'salir', "\n"
+    exit
+  else
+    puts 'Opcion no valida, ingresa una entre 1 y 4', "\n"
+  end
 end
